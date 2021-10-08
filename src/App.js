0753c61a495a3ef73d28bb6fd9f07c8471cf6e87
Braden Weber <bweber@celerik.com>
2021-10-07 22:09:45 -0400

    Server message

--- src/App.js
+++ src/App.js
@@ -6,6 +6,19 @@ class App extends Component {
     return (
       <React.Fragment>
         <Calculator />
+        <br/><br/>
+        <p style={{marginBottom: 2}}>
+          Server started 10/7/21 by&nbsp;
+          <a href='https://www.linkedin.com/in/braden-weber-6b04b0174/'>
+            Braden Weber
+          </a>
+        </p>
+        <p style={{marginTop: 0}}>
+          Project managed on&nbsp;
+          <a href='https://github.com/BradenLWeber/calculator'>
+            GitHub
+          </a>
+        </p>
       </React.Fragment>
     );
   }
