89029c7bec31facf676a0626a430393abfca9777
Braden Weber <bweber@celerik.com>
2021-10-18 08:06:36 -0400

    Update message

--- src/App.js
+++ src/App.js
@@ -8,16 +8,23 @@ class App extends Component {
         <Calculator />
         <br/><br/><br/>
         <div style={{backgroundColor: 'gray', height: 3, width: 481}} />
-        <p style={{marginBottom: 2, fontSize: 12}}>
-          Server started 10/7/21 by&nbsp;
-          <a href='https://www.linkedin.com/in/braden-weber-6b04b0174/'>
-            Braden Weber
-          </a>
-        </p>
+        <br/>
         <p style={{marginTop: 0, marginBottom: 2, fontSize: 12}}>
           Project managed on&nbsp;
           <a href='https://github.com/BradenLWeber/calculator'>
             GitHub
+          </a>
+        </p>
+        <p style={{marginTop: 0, marginBottom: 2, fontSize: 12}}>
+          Server hosted on&nbsp;
+          <a href='https://www.netlify.com/'>
+            Netlify
+          </a>
+        </p>
+        <p style={{marginTop: 0, marginBottom: 2, fontSize: 12}}>
+          Started on 10/7/21 and maintained by&nbsp;
+          <a href='https://www.linkedin.com/in/braden-weber-6b04b0174/'>
+            Braden Weber
           </a>
         </p>
         <br/>
